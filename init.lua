9485fb1d15ad33354a4e3923d58d582b65b834cc
Jesper Lindberg <utvecklarejesper@gmail.com>
2024-03-18 01:36:57 +0100

    added some lsp

--- init.lua
+++ init.lua
@@ -541,7 +541,9 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        black = {},
+        pylint = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
