aa1701cad002461cc82e63c6ddc97e5fda6439cd
Jesper Lindberg <utvecklarejesper@gmail.com>
2024-07-18 01:22:22 +0200

    added a script folder for custom scripts and a lua custom command folder

---
+++ scripts/python_scripts/table_formater.py
@@ -0,0 +1,52 @@
+import sys
+
+def adjust_table(table):
+    # Determine the maximum length of entries in each column
+    max_lengths = [max(len(str(item)) for item in col) for col in zip(*table)]
+    max_lengths[1] = max_lengths[1] - 1
+
+    # Adjust each entry to match the maximum length
+    adjusted_table = []
+    for row in table:
+        adjusted_row = []
+        for i, item in enumerate(row):
+            if item[0] != "|":
+                item = item.rstrip("|")
+                if "---" in str(item):
+                    item = item.rstrip(" ")
+                    adjusted_row.append(str(item).ljust(max_lengths[i] - 1, "-") + " |")
+                    continue
+                adjusted_row.append(str(item).ljust(max_lengths[i]) + "|")
+                continue
+
+            if "---" in str(item):
+                adjusted_row.append(str(item).ljust(max_lengths[i], "-").ljust(2))
+                continue
+            adjusted_row.append(str(item).ljust(max_lengths[i]))
+        adjusted_table.append(adjusted_row)
+
+    return adjusted_table
+
+
+def read_table():
+    input_text = sys.stdin.read()
+    lines = input_text.strip().split("\n")
+    table = [line.split(" | ") for line in lines]
+    return table
+
+def write_table(table):
+    output = ""
+    for row in table:
+        output = output + " | ".join(row) + "\n"
+    print(output)
+    
+
+def main():
+    table = read_table()
+    adjusted_table = adjust_table(table)
+    write_table(adjusted_table)
+
+
+
+if __name__ == "__main__":
+    main()
